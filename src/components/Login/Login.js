04c2d6a3f6811170a3ca6c9456322d801e1fa795
Awadhoot <>
2019-12-14 21:51:54 +0530

    Added font icons and basic login screen

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,57 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebookF, faGooglePlusG } from '@fortawesome/free-brands-svg-icons';
 
 import './Login.scss';
 export default class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div id='logreg-forms'>
+        <form className='form-signin'>
+          <h1 className='h3 mb-3 font-weight-normal text-center'> Sign in</h1>
+          <div className='social-login'>
+            <button className='btn facebook-btn social-btn' type='button'>
+              <span>
+                <FontAwesomeIcon icon={faFacebookF} />
+                {' Sign in with Facebook'}
+              </span>{' '}
+            </button>
+            <button className='btn google-btn social-btn' type='button'>
+              <span>
+                <FontAwesomeIcon icon={faGooglePlusG} /> {' Sign in with Google+'}
+              </span>{' '}
+            </button>
+          </div>
+          <p className='text-center'> OR </p>
+          <input
+            type='email'
+            id='inputEmail'
+            className='form-control'
+            placeholder='Email address'
+            required=''
+            autofocus=''
+          />
+          <input
+            type='password'
+            id='inputPassword'
+            className='form-control'
+            placeholder='Password'
+            required=''
+          />
+
+          <button className='btn btn-success btn-block' type='submit'>
+            <i className='fas fa-sign-in-alt'></i> Sign in
+          </button>
+          <a href='#' id='forgot_pswd'>
+            Forgot password?
+          </a>
+          <hr />
+          <button className='btn btn-primary btn-block' type='button' id='btn-signup'>
+            <i className='fas fa-user-plus'></i> Sign up New Account
+          </button>
+        </form>
+        <br />
+      </div>
+    );
   }
 }
