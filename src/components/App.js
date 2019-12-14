77abcd97ccfcb5deecb983fa9b23d385c703945e
Awadhoot <>
2019-12-14 23:17:30 +0530

    Added facebook login - login with facebook sdk

--- src/components/App.js
+++ src/components/App.js
@@ -1,44 +1,62 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.scss';
 import Albums from './Albums/Albums';
 import Login from './Login/Login';
 import PrivateRoute from './privateRoute';
 
-function App() {
-  return (
-    <div className='App container'>
-      {/* <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'>
-          Learn React
-        </a>
-      </header> */}
-      <Switch>
-        <Route path='/login'>
-          <Login />
-        </Route>
-        <PrivateRoute path='/facebook-albums' authenticated>
-          <Albums />
-        </PrivateRoute>
-        <Route path='/'>
-          <Redirect
-            to={{
-              pathname: '/facebook-albums'
-            }}
-          />
-        </Route>
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    authenticated: false
+  };
+  componentDidMount() {
+    if (localStorage.getItem('accessToken')) {
+      this.setState({ authenticated: true });
+    }
+  }
+
+  componentClicked = event => {
+    console.log('Clicked', event);
+  };
+  responseFacebook = event => {
+    console.log('RESPONSE ', event);
+  };
+  render() {
+    const { authenticated } = this.state;
+    return (
+      <div className='App container'>
+        {/* <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className='App-link'
+            href='https://reactjs.org'
+            target='_blank'
+            rel='noopener noreferrer'>
+            Learn React
+          </a>
+        </header> */}
+        <Switch>
+          <Route path='/login'>
+            <Login />
+          </Route>
+          <PrivateRoute path='/facebook-albums' authenticated={authenticated}>
+            <Albums />
+          </PrivateRoute>
+          <Route path='/'>
+            <Redirect
+              to={{
+                pathname: '/facebook-albums'
+              }}
+            />
+          </Route>
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,16 +1,35 @@
 import React from 'react';
+import FacebookLogin from 'react-facebook-login/dist/facebook-login-render-props';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebookF, faGooglePlusG } from '@fortawesome/free-brands-svg-icons';
 
 import './Login.scss';
 export default class Login extends React.Component {
+  logIn = event => {
+    console.log('Clicked', event);
+    if (window.FB) {
+      const { FB } = window;
+      FB.login(function(response) {
+        console.log('RES', response);
+        
+        if (response.authResponse) {
+          console.log('Welcome!  Fetching your information.... ');
+          FB.api('/me', function(response) {
+            console.log('Good to see you, ' + response.name + '.', response);
+          });
+        } else {
+          console.log('User cancelled login or did not fully authorize.');
+        }
+      });
+    }
+  };
   render() {
     return (
       <div id='logreg-forms'>
         <form className='form-signin'>
           <h1 className='h3 mb-3 font-weight-normal text-center'> Sign in</h1>
           <div className='social-login'>
-            <button className='btn facebook-btn social-btn' type='button'>
+            <button className='btn facebook-btn social-btn' type='button' onClick={this.logIn}>
               <span>
                 <FontAwesomeIcon icon={faFacebookF} />
                 {' Sign in with Facebook'}
