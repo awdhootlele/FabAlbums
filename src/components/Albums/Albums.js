fdc5608a42e19dcc94034358abf7da19bafdd646
Awadhoot <>
2019-12-14 21:10:28 +0530

    Added protected routes and basic router structure

---
+++ src/components/Albums/Albums.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+import './Albums.scss';
+export default class Albums extends React.Component {
+  render() {
+    return <div>ALBUMS</div>;
+  }
+}
--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,42 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import { Switch, Route, Redirect } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.scss';
+import Albums from './Albums/Albums';
+import Login from './Login/Login';
+import PrivateRoute from './privateRoute';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      {/* <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'>
           Learn React
         </a>
-      </header>
+      </header> */}
+      <Switch>
+        <Route path='/login'>
+          <Login />
+        </Route>
+        <PrivateRoute path='/facebook-albums' authenticated>
+          <Albums />
+        </PrivateRoute>
+        <Route path='/'>
+          <Redirect
+            to={{
+              pathname: '/facebook-albums'
+            }}
+          />
+        </Route>
+      </Switch>
     </div>
   );
 }
